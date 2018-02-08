726110d9d8d7e4d7f9e6926006656c357d293a62
Samantha Leung <samantha.leung6@shaw.ca>
2018-02-07 22:29:18 -0800

    Refactor Firebase API authentication calls into separate file

---
+++ auth/auth.js
@@ -0,0 +1,57 @@
+import firebase from '../config/firebase'
+
+
+
+export const checkLoginStatus = (callback) => {
+	firebase.auth().onAuthStateChanged((user) => {
+		callback(user)
+	})
+}
+
+
+
+
+export const onLogin = (email, password) => {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((user) => {
+      // If you need to do anything with the user, do it here
+      // The user will be logged in automatically by the 
+      // `onAuthStateChanged` listener we set up in App.js earlier
+    })
+    .catch((error) => {
+      const { code, message } = error;
+      console.log("Error: ", message)
+      // For details of error codes, see the docs
+      // The message contains the default Firebase string
+      // representation of the error
+    });
+}
+
+
+export const onRegister = (email, password) => {
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((user) => {
+      console.log("Hooray!", user)
+      // If you need to do anything with the user, do it here
+      // The user will be logged in automatically by the
+      // `onAuthStateChanged` listener we set up in App.js earlier
+    })
+    .catch((error) => {
+      const { code, message } = error;
+      console.log("Oh no", error)
+      // For details of error codes, see the docs
+      // The message contains the default Firebase string
+      // representation of the error
+    });
+}
+
+
+
+export const onLogout = () => {
+  firebase.auth().signOut()
+    .then(() => {
+      console.log("Logged out")
+  }).catch((error) => {
+    console.log("Oh no", error)
+  });
+}
