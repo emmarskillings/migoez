094b676075d75f9037e9019c182095c694f3bcab
Spencer Vatrt-Watts <asvw@uvic.ca>
2018-02-20 19:37:03 -0800

    Make map and logout button visible at the same time

--- src/LoggedIn.js
+++ src/LoggedIn.js
@@ -4,18 +4,42 @@
 */
 
 import React from 'react';
+import { StyleSheet, View, Text, Button, SafeAreaView } from 'react-native';
 import { MapView } from 'expo'
 
-const LoggedIn = () => (
+const LoggedIn = ({ onLogout }) => (
+	<SafeAreaView style={styles.container}> 
+    <View style={styles.button}>
+	    <Text>Testing thy log in screen</Text>
+			<Button onPress={onLogout} 
+				title="Log Out"
+			/>
+    </View>
     <MapView
-        style={{ flex: 1 }}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
+      style={ styles.map }
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
     />
+  </SafeAreaView>
 )
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  button: {
+  	flex: 1,
+  	justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  map: {
+    flex: 6,
+  },
+})
 
 export default LoggedIn
