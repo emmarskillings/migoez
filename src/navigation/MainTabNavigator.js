443b20fee504cec112193a82070d8d06952bd790
Spencer Vatrt-Watts <asvw@uvic.ca>
2018-03-28 17:36:28 -0700

    Put AddEvent page in tab navigation

--- src/navigation/MainTabNavigator.js
+++ src/navigation/MainTabNavigator.js
@@ -3,11 +3,15 @@ import { TabNavigator, TabBarBottom } from "react-navigation";
 
 import HomeScreen from "../scenes/LoggedIn";
 import Profile from "../scenes/Profile";
+import AddEvent from "../scenes/AddEvent/AddEvent";
 
 export default TabNavigator(
   {
     Home: {
       screen: HomeScreen, 
+    },
+    AddEvent: {
+      screen: AddEvent,
     },
     Profile: {
       screen: props => <Profile onLogout={props.screenProps.onLogout}/>
@@ -19,6 +23,8 @@ export default TabNavigator(
         const { routeName } = navigation.state;
         switch (routeName) {
           case "Home":
+            break;
+          case "AddEvent":
             break;
           case "Profile":
             break;
