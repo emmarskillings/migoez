4aba284b0e73f3dd069ebec95cbeae108982bef5
Samantha Leung <samantha.leung6@shaw.ca>
2018-04-15 21:22:57 -0700

    Bug fix for profile page navigation

--- src/scenes/LoggedIn.js
+++ src/scenes/LoggedIn.js
@@ -5,35 +5,42 @@ import Map from "./Map.js";
 import Profile from "./Profile.js";
 import AddEvent from "./AddEvent/AddEvent";
 
-export default TabNavigator(
-  {
-    Home: {
-      screen: Map
-    },
-    AddEvent: {
-      screen: AddEvent
-    },
-    Profile: {
-      screen: props => <Profile onLogout={props.screenProps.onLogout} />
-    }
-  },
-  {
-    navigationOptions: ({ navigation }) => ({
-      tabBarIcon: () => {
-        const { routeName } = navigation.state;
-        switch (routeName) {
-          case "Home":
-            break;
-          case "AddEvent":
-            break;
-          case "Profile":
-            break;
+const LoggedIn = ({ onLogout }) => {
+  const TabNavigation = TabNavigator(
+    {
+      Home: {
+        screen: Map
+      },
+      AddEvent: {
+        screen: AddEvent
+      },
+      Profile: {
+        screen: props => {
+          return <Profile onLogout={onLogout} />;
         }
       }
-    }),
-    tabBarComponent: TabBarBottom,
-    tabBarPosition: "bottom",
-    animationEnabled: true,
-    swipeEnabled: true
-  }
-);
+    },
+    {
+      navigationOptions: ({ navigation }) => ({
+        tabBarIcon: () => {
+          const { routeName } = navigation.state;
+          switch (routeName) {
+            case "Home":
+              break;
+            case "AddEvent":
+              break;
+            case "Profile":
+              break;
+          }
+        }
+      }),
+      tabBarComponent: TabBarBottom,
+      tabBarPosition: "bottom",
+      animationEnabled: true,
+      swipeEnabled: true
+    }
+  );
+
+  return <TabNavigation />;
+};
+export default LoggedIn;
