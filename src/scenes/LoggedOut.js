f24b7e1076d75c52c6447720235e26380c2c0988
Samantha Leung <samantha.leung6@shaw.ca>
2018-06-30 14:38:28 -0700

    Restyle login page

--- src/scenes/LoggedOut.js
+++ src/scenes/LoggedOut.js
@@ -1,59 +1,89 @@
 /**
-* LoggedOut React Component
-* 	User is not signed in
-*/
+ * LoggedOut React Component
+ * 	User is not signed in
+ */
 
 import React, { Component } from "react";
-import { StyleSheet, Text, TextInput, Button, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  Button,
+  View,
+  SafeAreaView
+} from "react-native";
+import TextBox from "./components/TextBox.js";
 
 class LoggedOut extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			email: "",
-			password: ""
-		}
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
 
-	render() {
-		const { onRegister, onLogin } = this.props
+  render() {
+    const { onRegister, onLogin } = this.props;
 
-		return (
-		<SafeAreaView style={styles.container}> 
-			<Text>Migoez</Text>
-			<TextInput style={styles.textInput} 
-					   placeholder="Email"
-					   onChangeText={(text) => this.setState({ email: text })}
-					   value={this.state.email}
-			/>
-			<TextInput style={styles.textInput} 
-					   placeholder="Password"
-					   onChangeText={(text) => this.setState({ password: text })}
-					   value={this.state.password}
-			/>
-			<Button onPress={() => onLogin(this.state.email, this.state.password)} 
-					title="Log In"
-			/>
-			<Button onPress={() => onRegister(this.state.email, this.state.password)} 
-					title="Register"
-			/>
-		</SafeAreaView>)
-	}
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.title}>Migoez</Text>
+        <TextBox
+          placeholder={"Email"}
+          onChangeText={text => {
+            this.setState({ email: text });
+          }}
+          value={this.state.email}
+        />
+        <TextBox
+          placeholder="Password"
+          onChangeText={text => this.setState({ password: text })}
+          value={this.state.password}
+        />
+        <View style={styles.buttonsContainer}>
+          <ButtonView
+            onPress={() => onLogin(this.state.email, this.state.password)}
+            title="Log In"
+          />
+          <ButtonView
+            onPress={() => onRegister(this.state.email, this.state.password)}
+            title="Register"
+          />
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
+const ButtonView = ({ onPress, title }) => (
+  <View style={styles.button}>
+    <Button onPress={onPress} title={title} />
+  </View>
+);
+
 const styles = StyleSheet.create({
+  title: {
+    textAlign: "left",
+    fontWeight: "bold",
+    fontSize: 25,
+    marginBottom: 20
+  },
+
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#F5FCFF",
+    backgroundColor: "#F5FCFF"
+  },
+  button: {
+    marginTop: 5
   },
 
-  textInput: {
-  	margin: 2,
-  	borderColor: "#000000", 
-  	borderWidth: 1
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center"
   }
-})
+});
 
 export default LoggedOut;
