72dd403f1500c0b103eb17e739a9d6d0e7457a80
Emma Skillings <emmarskillings@gmail.com>
2018-09-29 13:36:58 -0400

    new firebase added

--- src/api/config.js
+++ src/api/config.js
@@ -2,13 +2,13 @@ import * as firebase from "firebase";
 
 // Initialize Firebase
 const firebaseConfig = {
-   apiKey: "AIzaSyAjzO91d3AUV2fuDDtMXe8h81FMYxttYJQ",
-   authDomain: "migoez-1337.firebaseapp.com",
-   databaseURL: "https://migoez-1337.firebaseio.com",
-   projectId: "migoez-1337",
-   storageBucket: "migoez-1337.appspot.com",
-   messagingSenderId: "734144645212"
- };
+  apiKey: "AIzaSyDcrd2P3bIkNL3inl1VEKyE4oMpOHVLk-M",
+  authDomain: "migoez-9be0e.firebaseapp.com",
+  databaseURL: "https://migoez-9be0e.firebaseio.com",
+  projectId: "migoez-9be0e",
+  storageBucket: "migoez-9be0e.appspot.com",
+  messagingSenderId: "191751734968"
+};
 
 firebase.initializeApp(firebaseConfig);
 
